f5e38caf59181456f49891dd8c57b8d434ad4a4b
AYU <jhaayushi1102@gmail.com>
2026-08-22 13:42:45 +0530

    feat: initialize Spring Boot backend

---
+++ src/main/java/com/conceptlens/HelloController.java
@@ -0,0 +1,13 @@
+package com.conceptlens;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "ConceptLens backend is running!";
+    }
+}
