dc3ba8d941921346207ad8595a8dd9e973a06637
AYU <jhaayushi1102@gmail.com>
2026-08-22 19:51:39 +0530

    feat: add concept domain model

---
+++ src/main/java/com/conceptlens/Concept.java
@@ -0,0 +1,31 @@
+package com.conceptlens;
+
+public class Concept {
+    
+    private String name;
+    private String description;
+
+    public Concept() {
+    }
+
+    public Concept(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
